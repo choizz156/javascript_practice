802f6f25dde5f0925887ebe7e664e49450bd05ea
choizz156 <tkfka156@gmail.com>
2026-07-24 14:25:44 +0900

    feat: 여행지 추가 폼 및 양방향 바인딩 맛보기 구현

--- src/main.js
+++ src/main.js
@@ -8,30 +8,52 @@ function CityList($app) {
   ];
 
   const render = () => {
-    const listItems = state
-      .map((item) => `<li> ${item.name}(${item.score})</li>`)
-      .join("");
+    const listItems = state.map((item) => CityItem(item)).join("");
 
     $app.innerHTML = `
     <div>추천 여행지</div>
     <ul>
       ${listItems}
     </ul>
-    <button id="update-btn">서울 점수 올리기!</button>
+    <form id="add-form">
+      <input type="text" id="new-city" placeholder="새 여행지 이름" />
+      <button type="submit">추가하기</button>
+    </form>
     `;
   };
 
   $app.addEventListener("click", (e) => {
-    if (e.target.id === "update-btn") {
-      state[0].score += 0.1;
+    //  e.target.classList.contains("like-btn")
+    if (e.target.className === "like-btn") {
+      const clickedName = e.target.dataset.name;
+      const city = state.find((item) => item.name === clickedName);
+      city.score = +(city.score + 0.1).toFixed(1); // 부동소수
       render();
+    }
+  });
+
+  $app.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (e.target.id === "add-form") {
+      const inputValue = document.querySelector("#new-city").value;
+      if (inputValue.trim() !== "") {
+        state.push({ name: inputValue, score: 0 });
+        render();
+      }
     }
   });
 
   render();
 }
 
-CityList($app)
+//컴포넌트는 함수도 대문자로 쓴다.
+function CityItem(item) {
+  return `<li> ${item.name}(${item.score})</li>
+     <button class="like-btn" data-name="${item.name}">좋아요!!</button>`;
+}
+
+//클로져 적용
+CityList($app);
 
 // const $div = document.createElement("div");
 // $div.textContent = "추천 여행지df";
